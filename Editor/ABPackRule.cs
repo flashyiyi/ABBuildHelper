b7171bf57387e5718256f22d079e9a96bbbabaf8
flashyiyi <yiyiflashstudio@163.com>
2018-03-22 13:49:35 +0800

    将ABPackRuleConfig拆成单独文件

--- Editor/ABPackRule.cs
+++ Editor/ABPackRule.cs
@@ -233,27 +233,5 @@ namespace ABBuildHelper
             }
         }
     }
-
-    [Serializable]
-    public class ABPackRuleConfig : ScriptableObject
-    {
-        [Serializable]
-        public class Rule
-        {
-            public string path;
-            public string typeFilter;
-            public int ruleType;
-
-            public bool MatchType(string type)
-            {
-                if (type == "MonoScript" || type == "DefaultAsset")
-                    return false;
-
-                return string.IsNullOrEmpty(typeFilter) ? true : Array.IndexOf(typeFilter.Split(','), type) >= 0;
-            }
-        }
-        public List<Rule> rules = new List<Rule>();
-    }
-
 }
 
---
+++ Editor/ABPackRuleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[Serializable]
+public class ABPackRuleConfig : ScriptableObject
+{
+    [Serializable]
+    public class Rule
+    {
+        public string path;
+        public string typeFilter;
+        public int ruleType;
+
+        public bool MatchType(string type)
+        {
+            if (type == "MonoScript" || type == "DefaultAsset")
+                return false;
+
+            return string.IsNullOrEmpty(typeFilter) ? true : Array.IndexOf(typeFilter.Split(','), type) >= 0;
+        }
+    }
+    public List<Rule> rules = new List<Rule>();
+}
